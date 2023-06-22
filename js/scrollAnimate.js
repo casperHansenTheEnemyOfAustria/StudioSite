ba7e175c4560490bf3bc4fee83a6826445a8b9cb
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2023-06-22 22:47:24 +0200

    more style additions and siuxing also animations

---
+++ js/scrollAnimate.js
@@ -0,0 +1,35 @@
+function isElementInViewport(el) {
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+      el = el[0];
+    }
+    var rect = el.getBoundingClientRect();
+    return (
+      (rect.top <= 0
+        && rect.bottom >= 0)
+      ||
+      (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+      ||
+      (rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    );
+  }
+
+var scroll = window.requestAnimationFrame ||
+    function(callback) {window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll(".onscroll");
+// var elementsToShow = document.querySelectorAll("li");
+function loop() {
+    elementsToShow.forEach(function (element) { 
+        if (isElementInViewport(element)) {
+            element.classList.add("animation")
+        }
+        else {
+            element.classList.remove("animation")
+        }
+    });
+    scroll(loop);
+}
+
+loop();
+
